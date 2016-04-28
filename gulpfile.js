a2fba99bb24d2e5e6f2c5e9ea5be437062de69f9
Travis <mail@travisbloomfield.com>
2016-04-28 11:24:50 +1000

    Add gulpfile.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+// Set up BroswerSync
+gulp.task('browserSync', function() {
+  browserSync.init({});
+});
+
+// Compile SASS
+gulp.task('sass', function() {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+// Watch SASS folder and index.html for changes and then reload.
+gulp.task('watch', ['browserSync'], function() {
+  gulp.watch('./sass/**/*.scss', ['sass', browserSync.reload]);
+  gulp.watch('./index.html', browserSync.reload());
+});
